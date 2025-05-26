a62dc2e5fbd5fd50a9812c89ce02860a4f5e6108
house79-gex <75929529+house79-gex@users.noreply.github.com>
2025-05-26 20:55:53 +0200

    Update main_window.py

--- EvoWood/ui/main_window.py
+++ EvoWood/ui/main_window.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMainWindow, QAction, QLabel, QVBoxLayout, QWidget, QStatusBar
+from .clienti_window import ClientiWindow
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -29,13 +30,14 @@ class MainWindow(QMainWindow):
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
 
-        # Moduli (placeholder per futuri moduli)
+        # Moduli
         modules_menu = menubar.addMenu("Moduli")
         rilievo_action = QAction("Rilievo Misure", self)
         modules_menu.addAction(rilievo_action)
         cad_action = QAction("CAD", self)
         modules_menu.addAction(cad_action)
         clienti_action = QAction("Gestione Clienti", self)
+        clienti_action.triggered.connect(self.open_clienti_window)
         modules_menu.addAction(clienti_action)
 
         # Help
@@ -43,6 +45,10 @@ class MainWindow(QMainWindow):
         about_action = QAction("Info su EvoWood", self)
         about_action.triggered.connect(self.show_about)
         help_menu.addAction(about_action)
+
+    def open_clienti_window(self):
+        self.clienti_window = ClientiWindow()
+        self.clienti_window.show()
 
     def show_about(self):
         from PyQt5.QtWidgets import QMessageBox
